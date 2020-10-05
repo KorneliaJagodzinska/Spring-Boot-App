0475ec14b265028c743ad6132be4f7004825570d
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-05 19:30:24 +0200

    CarApi

--- src/main/java/com/example/demo/CarApi.java
+++ src/main/java/com/example/demo/CarApi.java
@@ -1,14 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CarApi {
+    @Autowired
+    private CarManager carManager;
 
-    @GetMapping("/hello")
-    private String sayHello(){
-        return "Hello";
+    @GetMapping("/getCars")
+    public List<Car> getCars() {
+        return carManager.getCarList();
+    }
 
+    @PostMapping("/addCar")
+    public List<Car> addCar(@RequestBody Car car) {
+        return carManager.addCar(car);
     }
 }
--- src/main/java/com/example/demo/CarManager.java
+++ src/main/java/com/example/demo/CarManager.java
@@ -5,13 +5,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Service
 public class CarManager {
     private List<Car> carList;
 
-
+    private CarManager() {
+        this.carList = new ArrayList<>();
+        carList.add(new Car("Polo", "VW"));
+        carList.add(new Car("126p", "Fiat"));
+    }
+    public List<Car> addCar(Car car){
+         carList.add(car);
+         return this.carList;
+    }
 }
