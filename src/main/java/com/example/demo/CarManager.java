833c7b45825281ea1d00bbca737edba947c4defb
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-05 19:05:16 +0200

    CarApi

--- src/main/java/com/example/demo/CarManager.java
+++ src/main/java/com/example/demo/CarManager.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Service
 public class CarManager {
     private List<Car> carList;
 
